3b4825303d1d1971a283a40ea522d6cc787ab93d
Jonas Gabriel <jga@unwire.dk>
2022-01-19 00:33:18 +0100

    Initial Commit

---
+++ src/main/java/net/somewhere/jenkinslabratapp/JenkinsLabratAppApplication.java
@@ -0,0 +1,13 @@
+package net.somewhere.jenkinslabratapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsLabratAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsLabratAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/somewhere/jenkinslabratapp/JenkinsLabratAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.somewhere.jenkinslabratapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsLabratAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
